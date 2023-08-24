e11d9c89224786a4d704ab8a9817b5967ed49a52
kartiksharma131 <51881502+kartiksharma131@users.noreply.github.com>
2023-08-24 17:37:14 +0530

    Minor project
    project skeleton

---
+++ Library-management-system-minorProject/src/test/java/com/minorProject/LibrarymanagementsystemminorProject/LibraryManagementSystemMinorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minorProject.LibrarymanagementsystemminorProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemMinorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
